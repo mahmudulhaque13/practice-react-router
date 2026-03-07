a8ab2d5c5f65ed42d7bd1ada09d8cd5d54d1ab7a
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-07 23:30:22 +0600

    router setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 import Contact from "./pages/Contact";
 
 function App() {
@@ -9,12 +11,14 @@ function App() {
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> |<Link to="/about">About</Link> |
-        <Link to="/contact">Contact</Link>
+        <Link to="/products">Products</Link> |<Link to="/contact">Contact</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetails />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
