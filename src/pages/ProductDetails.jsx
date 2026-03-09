89b65d9fc24f9b72102235b96b7b6cb12292c55d
mahmudulhaque13 <m.h.upal13@gmail.com>
2026-03-09 23:12:05 +0600

    productdetails added

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+const products = [
+  { id: 1, name: "Laptop", price: 1000 },
+  { id: 2, name: "Phone", price: 500 },
+  { id: 3, name: "Headphone", price: 200 },
+];
+
 const ProductDetails = () => {
-  const { id } = useParams;
-  return <h2>Product ID: {id}</h2>;
+  const { id } = useParams();
+
+  const product = products.find((p) => p.id === Number(id));
+
+  return (
+    <div>
+      <h2>{product.name}</h2>
+      <p>Price: ${product.price}</p>
+    </div>
+  );
 };
 
 export default ProductDetails;
